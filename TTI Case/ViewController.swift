de711d14d0a9e54fc2828adaa2e23e6fe8166a9c
Ömer Hamid Kamışlı <me@ohkamisli.dev>
2022-09-30 15:30:48 -0700

    Initial Commit

---
+++ TTI Case/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TTI Case
+//
+//  Created by Ömer Hamid Kamışlı on 9/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
