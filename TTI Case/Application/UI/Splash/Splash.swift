fd6668637b2ac059607d04e772dd25a771494905
Ömer Hamid Kamışlı <me@ohkamisli.dev>
2022-09-30 20:29:28 -0700

    Splash gradient added.

--- TTI Case/Application/UI/Splash/Splash.swift
+++ TTI Case/Application/UI/Splash/Splash.swift
@@ -9,8 +9,10 @@ import Foundation
 import UIKit
 
 class Splash: UIViewController {
+    private let gradient = CAGradientLayer()
     
     override func viewDidLoad() {
+        animateGradient()
         checkConnection()
     }
     
@@ -19,7 +21,9 @@ class Splash: UIViewController {
 // MARK: Functions
 extension Splash {
     func checkConnection(){
-        DispatchQueue.main.async{
+        DispatchQueue.main.async{ [weak self] in
+            guard let self = self else { return }
+            
             if NetworkMonitor.shared.isConnected {
                 Logger.shared.addLog(message: "Connected to network. App should be start")
             } else {
@@ -28,6 +32,44 @@ extension Splash {
                     exit(-1)
                 }))
             }
+        }
+    }
+    
+    private func animateGradient() {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            // create the gradient layer
+            self.gradient.frame = self.view.frame
+            self.gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+            self.gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+            self.gradient.colors = [
+                UIColor.white.cgColor,
+                UIColor(named: "SoftPrimary")?.cgColor ?? UIColor.white.cgColor,
+                UIColor(named: "Primary")?.cgColor ?? UIColor.white.cgColor
+            ]
+            
+            self.gradient.locations =  [1, 0.5, 0, 0.5, 1.5]
+            
+            let animation = CABasicAnimation(keyPath: #keyPath(CAGradientLayer.colors))
+            
+            animation.fromValue = [
+                UIColor(named: "SoftPrimary")?.cgColor  ?? UIColor.white.cgColor,
+                UIColor(named: "SoftPrimary")?.cgColor  ?? UIColor.white.cgColor,
+                UIColor(named: "Primary")?.cgColor  ?? UIColor.white.cgColor
+            ]
+            
+            animation.toValue = [
+                UIColor(named: "Primary")?.cgColor  ?? UIColor.white.cgColor,
+                UIColor(named: "SoftPrimary")?.cgColor ?? UIColor.white.cgColor,
+                UIColor(named: "Primary")?.cgColor ?? UIColor.white.cgColor
+            ]
+            
+            animation.duration = 1
+            animation.autoreverses = true
+            animation.repeatCount = .infinity
+            
+            self.gradient.add(animation, forKey: nil)
+            self.view.layer.insertSublayer(self.gradient, at: 0)
         }
     }
 }
